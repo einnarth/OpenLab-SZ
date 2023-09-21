4b7e5ed9eb89fe58e5ca21d6421af22d43c36124
samuelmalik <113421307+samuelmalik@users.noreply.github.com>
2023-09-21 08:22:31 +0200

    add User's properties and generate migration script

---
+++ OpenLab-SZ/Data/Migrations/20230921061255_UserProperties.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenLab_SZ.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Guild",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Xp",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Guild",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Xp",
+                table: "AspNetUsers");
+        }
+    }
+}
--- OpenLab-SZ/Models/ApplicationUser.cs
+++ OpenLab-SZ/Models/ApplicationUser.cs
@@ -4,6 +4,8 @@ namespace OpenLab_SZ.Models;
 
 public class ApplicationUser : IdentityUser
 {
+    public string Guild {  get; set; }
+    public int Xp {  get; set; }
     
 }
 
