19723bb5b638d3aadf66e26fafd307a7ba8b57a0
samuelmalik <113421307+samuelmalik@users.noreply.github.com>
2023-09-26 08:17:41 +0200

    create data.service

--- OpenLab-SZ/ClientApp/src/app/dashboard/dashboard.component.ts
+++ OpenLab-SZ/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './../../data.service';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -11,5 +13,16 @@ export class DashboardComponent {
   xp: number = 100;
   requiredXp: number = 120;
   progress: number = Math.floor(this.xp / this.requiredXp * 100);
+
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    // Call the fetchData method to retrieve data
+    this.dataService.fetchData().subscribe(data => {
+      // Handle the data here
+      console.log(data);
+    });
+  }
 
 }
